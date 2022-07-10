8610f17c12804eda1d8111409be1c37f64491c6e
Joe Tang <joetang81@gmail.com>
2022-07-10 14:10:32 -0400

    Implement About Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'about/index', to: "about#index"
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
+
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
+
+
   end
 
   resources :orders, only: [:create, :show]
